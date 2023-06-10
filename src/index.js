043bd190ab26fd05b26ecbb95bfbd77f6785677b
emiliano alfaraz <emialfa2@gmail.com>
2023-06-10 15:40:53 -0300

    add express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // APIs
 const openaiApi = require("./config/openai.config");
