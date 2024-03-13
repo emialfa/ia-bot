879604bec3bf1c61e0466ab501e823e33722c205
Emiliano M Alfaraz <71176986+emialfa@users.noreply.github.com>
2024-03-13 14:07:28 -0300

    Adding socket.io connection url

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const http = require("http");
-const cors = require('cors');
-const logger = require('morgan');
-const path = require('path');
+const cors = require("cors");
+const logger = require("morgan");
+const path = require("path");
 const { Server: SocketServer } = require("socket.io");
-const chatsRouter = require('./routes/chat.routes');
-const botsRouter = require('./routes/bot.routes');
+const chatsRouter = require("./routes/chat.routes");
+const botsRouter = require("./routes/bot.routes");
 
 const app = express();
 
@@ -13,24 +13,27 @@ const server = http.createServer(app);
 const port = process.env.PORT || 5000;
 
 const io = new SocketServer(server, {
-   cors: process.env.NODE_ENV === "development" ? {
-     origin: "http://127.0.0.1:5173",
-   } : {},
+  path: "/hair-questionary/api/socket.io",
+  cors:
+    process.env.NODE_ENV === "development"
+      ? {
+          origin: "http://127.0.0.1:5173",
+        }
+      : {},
 });
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/hair-questionary/api/chats', chatsRouter);
-app.use('/hair-questionary/api/bots', botsRouter);
-app.use(logger('dev'));
+app.use("/hair-questionary/api/chats", chatsRouter);
+app.use("/hair-questionary/api/bots", botsRouter);
+app.use(logger("dev"));
 
-app.use('/hair-questionary/', express.static(path.join(__dirname, 'dist')));
+app.use("/hair-questionary/", express.static(path.join(__dirname, "dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 server.listen(port, () => {
