c7b5c6458b9824d7a649c83923b822996bc26579
Emiliano M Alfaraz <71176986+emialfa@users.noreply.github.com>
2024-11-03 20:45:07 +0100

    Adding logs endpoint

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { Server: SocketServer } = require("socket.io");
 const chatsRouter = require("./routes/chat.routes");
 const botsRouter = require("./routes/bot.routes");
 const expressIP = require('express-ip');
-
+const fs = require("fs");
 const app = express();
 
 app.use(expressIP().getIpInfoMiddleware);
@@ -36,13 +36,76 @@ const io = new SocketServer(server, {
   randomizationFactor: 0.5,                  // Para diversificar los intentos
 });
 
+function getTimestamp() {
+  const now = new Date();
+  return now.toLocaleString("es-ES", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+}
+
+// Mapea códigos de color ANSI a colores CSS
+const colorMapping = {
+  "\x1b[32m": "green", // Verde
+  "\x1b[31m": "#c71010", // Rojo
+  "\x1b[33m": "#b9882e", // Naranja
+  "\x1b[36m": "#247272",  // Cian
+};
+
+// Función para convertir códigos ANSI a HTML
+function ansiToHtml(logText) {
+  // Detecta si hay algún código de color ANSI en el texto
+  const match = logText.match(/\x1b\[(3[1-7])m/);
+  
+  if (match && colorMapping[match[0]]) {
+    // Si hay código ANSI, envuelve el texto en un <span> con el color correspondiente
+    const color = colorMapping[match[0]];
+    const cleanText = logText.replace(/\x1b\[\d+m/g, "").replace("%s", ""); // Elimina los códigos ANSI del texto
+    return `<span style="color: ${color};">${cleanText}</span>`;
+  }
+  
+  // Si no hay código ANSI, devuelve el texto tal cual
+  return logText;
+}
+
+const logPath = path.join(__dirname, "access.log");
+const logStream = fs.createWriteStream(logPath, { flags: "a" });
+
+app.use(logger("dev"));
+// app.use(logger("dev", { stream: logStream }));
+const originalConsoleLog = console.log;
+console.log = (...args) => {
+  const timestampedMessage = `[${getTimestamp()}] ${args.join(" ")}`;
+  originalConsoleLog(timestampedMessage); // Muestra en consola con la fecha y hora
+  logStream.write(ansiToHtml(timestampedMessage) + "\n"); // Escribe en el archivo de log con la fecha y hora
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/hair-questionary/api/chats", chatsRouter);
 app.use("/hair-questionary/api/bots", botsRouter);
-app.use(logger("dev"));
+app.get("/hair-questionary/api/logs", (req, res) => {
+  fs.readFile(logPath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error al cargar el archivo de logs.");
+      return;
+    }
+    res.send(`
+      <html>
+        <head><title>Logs</title></head>
+        <body style="font-family: monospace; white-space: pre-wrap;">
+          ${data}
+        </body>
+      </html>
+    `);
+  });
+});
 
 app.use("/hair-questionary/", express.static(path.join(__dirname, "dist")));
 
